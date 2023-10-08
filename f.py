4d65ee3d98eac22b84585f5bdc51bc7cf7393968
Milky47Way <145270243+Milky47Way@users.noreply.github.com>
2023-10-08 17:26:13 +0300

    Add files via upload

---
+++ f.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import  QApplication
+
+app = QApplication([])
+
+from m2 import *
+
+
+window.show()
+app.exec_()
---
+++ f2.py
@@ -0,0 +1,78 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QButtonGroup, QRadioButton, QPushButton, QLabel, QSpinBox, QApplication
+
+window = QWidget()
+
+btn_menu = QPushButton('Меню')
+btn_rest = QPushButton('Відпочити')
+btn_next = QPushButton('Відповісти')
+
+rb_ans1 = QRadioButton('1')
+rb_ans2 = QRadioButton('2')
+rb_ans3 = QRadioButton('3')
+rb_ans4 = QRadioButton('4')
+
+RadioGroup = QButtonGroup()
+RadioGroup.addButton(rb_ans1)
+RadioGroup.addButton(rb_ans2)
+RadioGroup.addButton(rb_ans3)
+RadioGroup.addButton(rb_ans4)
+
+lb_question = QLabel('Запитання')
+lb_rest = QLabel('Хвилин')
+lb_result = QLabel('Правильно')
+lb_right_answer = QLabel('Відповідь')
+
+sp_rest = QSpinBox()
+gb_question = QGroupBox('Варіанти відповідей')
+
+rb_v1 = QVBoxLayout()
+rb_v2 = QVBoxLayout()
+rb_h1 = QVBoxLayout()
+
+rb_v1.addWidget(rb_ans1)
+rb_v1.addWidget(rb_ans2)
+rb_v2.addWidget(rb_ans3)
+rb_v2.addWidget(rb_ans4)
+
+rb_h1.addLayout(rb_v1)
+rb_h1.addLayout(rb_v2)
+gb_question.setLayout(rb_h1)
+
+gb_answer = QGroupBox()
+v1 = QVBoxLayout()
+v1.addWidget(lb_result)
+v1.addWidget(lb_right_answer)
+gb_answer.setLayout(v1)
+
+h1_main = QHBoxLayout()
+h2_main = QHBoxLayout()
+h3_main = QHBoxLayout()
+h4_main = QHBoxLayout()
+v1_main = QVBoxLayout()
+
+h1_main.addWidget(btn_menu)
+h1_main.addStretch(1)
+h1_main.addWidget(btn_rest)
+h1_main.addWidget(sp_rest)
+h1_main.addWidget(sp_rest)
+h1_main.addWidget(lb_rest)
+
+h2_main.addWidget(lb_result,  stretch = (Qt. AlignHCenter | Qt. AlignVCenter))
+h3_main.addWidget(gb_answer)
+h3_main.addWidget(gb_question)
+gb_answer.hide()
+
+h4_main.addStretch(1)
+h4_main.addWidget(btn_next, stretch=2)
+h4_main.addStretch(1)
+
+v1_main.addLayout(h1_main, stretch=1)
+v1_main.addLayout(h2_main, stretch=2)
+v1_main.addLayout(h3_main, stretch=8)
+v1_main.addLayout(h4_main)
+v1_main.setSpacing(5)
+
+v1.main.setLayout(v1_main)
+window.resize(550, 450)
+
